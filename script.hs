71ddef7312df8dc7c83ad3212b14109488fa2de5
Vo Minh Thu <thu@hypered.io>
2013-12-22 15:40:43 +0100

    Start the HTTP-proxying part.

--- script.hs
+++ script.hs
@@ -6,6 +6,7 @@ import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString.Lazy.Char8 as LC
 import qualified Data.ByteString.UTF8 as U
 import Data.Char (intToDigit)
+import Data.List (intersperse)
 import Data.Maybe (fromMaybe, maybeToList)
 import Network.HTTP
 import Network.Stream
@@ -14,48 +15,105 @@ import qualified Data.ByteString.Base64 as Base64
 import Network.XmlRpc.Server 
 import Network.XmlRpc.Internals
 import Snap (liftIO)
-import Snap.Core hiding (methods, rqMethod, rqURI, POST, Request)
+import Snap.Core hiding (methods, getHeaders, rqMethod, rqURI, GET, POST, Request, Response)
+import qualified Snap.Core as Snap
 import Snap.Util.FileServe
 import Snap.Http.Server
+import Web.Cookie
 
 main :: IO ()
 main = quickHttpServe site
 
 site :: Snap ()
 site = route
-  [ ("xmlrpc/common", commonHandler)
-  -- [ ("xmlrpc/common", fallthrough "/xmlrpc/common")
-  , ("xmlrpc/object", fallthrough "/xmlrpc/object")
-  ]
+  [ ("xmlrpc/common", xmlrpcHandler "xmlrpc/common")
+  , ("xmlrpc/object", xmlrpcFallthrough "xmlrpc/object")
+  ] <|> httpFallthrough
 
-commonHandler :: Snap ()
-commonHandler = do
+-- | Try to handle the XML-RPC call direclty, or forward it to the
+-- openerp-server.
+xmlrpcHandler :: String -> Snap ()
+xmlrpcHandler to = do
   body <- LC.unpack <$> readRequestBody (1024 * 1024) -- TODO Is it enough ?
-  bs <- liftIO $ handleCall commonMethods body
-  writeLBS bs
-
-fallthrough :: String -> Snap ()
-fallthrough to = do
-  body <- LC.unpack <$> readRequestBody (1024 * 1024) -- TODO Is it enough ?
-  c <- liftIO $ handleError (return . Left . Fault 0) (Right <$> parseCall body)
-  case c of
+  mc <- liftIO $ handleError (return . Left . Fault 0) (Right <$> parseCall body)
+  case mc of
     Left e -> writeLBS $ renderResponse e
-    Right c' -> do
-      response <- liftIO $ post ("http://trunk-27602.runbot.openerp.com:9335" ++ to) [] $ renderCall c'
-      -- writeLBS $ renderResponse $ parseResponse response
-      writeLBS $ LC.pack response
+    Right mc' -> case mc' of
+      MethodCall "login" _ -> do
+        bs <- rpc login mc'
+        writeLBS $ renderResponse bs
+      _ -> forward mc' to
 
-commonMethods :: MethodCall -> ServerResult
-commonMethods = methods
-  [ ("login", fun login)
-  ]
+-- | Forward the XML-RPC call to the openerp-server, without trying to handle
+-- it directly.
+xmlrpcFallthrough :: String -> Snap ()
+xmlrpcFallthrough to = do
+  body <- LC.unpack <$> readRequestBody (1024 * 1024) -- TODO Is it enough ?
+  mc <- liftIO $ handleError (return . Left . Fault 0) (Right <$> parseCall body)
+  case mc of
+    Left e -> writeLBS $ renderResponse e
+    Right mc' -> forward mc' to
+
+forward c to = do
+  response <- liftIO $ post ("http://trunk-27602.runbot.openerp.com:9335/" ++ to) [] [] $ renderCall c
+  -- writeLBS $ renderResponse $ parseResponse response
+  writeBS $ rspBody response
+
+rpc f = liftIO . handleError (return . Fault 0) . (fst . fun) f
+
+httpFallthrough :: Snap ()
+httpFallthrough =
+  method Snap.POST postFallthrough <|>
+  method Snap.GET getFallthrough
+
+postFallthrough :: Snap ()
+postFallthrough = do
+  uri <- Snap.rqURI <$> Snap.getRequest
+  cookies <- Snap.rqCookies <$> Snap.getRequest
+  liftIO $ print cookies
+  body <- LC.unpack <$> readRequestBody (1024 * 1024) -- TODO Is it enough ?
+  let to = C.unpack uri
+  response <- liftIO $ post ("http://trunk-27602.runbot.openerp.com:9335" ++ to) [] cookies $ LC.pack body
+  addCookies response
+  writeBS $ rspBody response
+
+getFallthrough :: Snap ()
+getFallthrough = do
+  req <- Snap.getRequest
+  uri <- Snap.rqURI <$> Snap.getRequest
+  cookies <- Snap.rqCookies <$> Snap.getRequest
+  liftIO $ print req
+  liftIO $ print uri
+  liftIO $ print cookies
+  let to = C.unpack uri
+  response <- liftIO $ get ("http://trunk-27602.runbot.openerp.com:9335" ++ to) [] cookies
+  addCookies response
+  writeBS $ rspBody response
+
+addCookies response = do
+  let cookies' = filter ((HdrSetCookie ==) . hdrName) $ getHeaders response
+      mkCookie (Header HdrSetCookie c) = Cookie
+        { cookieName = setCookieName c'
+        , cookieValue = setCookieValue c'
+        , cookieExpires = setCookieExpires c'
+        , cookieDomain = setCookieDomain c'
+        , cookiePath = setCookiePath c'
+        , cookieHttpOnly = setCookieHttpOnly c'
+        , cookieSecure = setCookieSecure c'
+        }
+        where c' = parseSetCookie $ C.pack c
+  mapM (modifyResponse . addResponseCookie . mkCookie) cookies'
+
+----------------------------------------------------------------------
+-- XML-RPC methods handled directly by openerpfront.
+----------------------------------------------------------------------
 
 login :: String -> String -> String -> IO Int
 login databaseName login password = return 1
 
 
 ----------------------------------------------------------------------
--- This is code from HaXR, not exported from Network.XmlRpc.Client.
+-- This is (modified) code from HaXR, not exported from Network.XmlRpc.Client.
 ----------------------------------------------------------------------
 
 userAgent :: String
@@ -65,24 +123,22 @@ handleE :: Monad m => (ConnError -> m a) -> Either ConnError a -> m a
 handleE h (Left e) = h e
 handleE _ (Right v) = return v
 
-post :: String -> [Header] -> LC.ByteString -> IO String
-post url headers content = do
+post :: String -> [Header] -> [Cookie] -> LC.ByteString -> IO (Response U.ByteString)
+post url headers cookies content = do
     uri <- maybeFail ("Bad URI: '" ++ url ++ "'") (parseURI url)
     let a = uriAuthority uri
     auth <- maybeFail ("Bad URI authority: '" ++ show (fmap showAuth a) ++ "'") a
-    post_ uri auth headers content
+    post_ uri auth headers' content
   where showAuth (URIAuth u r p) = "URIAuth "++u++" "++r++" "++p
+        headers' = Header HdrCookie (concat $ intersperse ";" $ map showCookie cookies) : headers
 
-post_ :: URI -> URIAuth -> [Header] -> LC.ByteString -> IO String
+showCookie c = C.unpack $ C.concat [cookieName c, "=", cookieValue c]
+
+post_ :: URI -> URIAuth -> [Header] -> LC.ByteString -> IO (Response U.ByteString)
 post_ uri auth headers content = do
     eresp <- simpleHTTP (request uri auth headers (C.concat . LC.toChunks $ content))
     resp <- handleE (fail . show) eresp
-    case rspCode resp of
-                      (2,0,0) -> return (U.toString (rspBody resp))
-                      _ -> fail (httpError resp)
-    where
-    showRspCode (a,b,c) = map intToDigit [a,b,c]
-    httpError resp = showRspCode (rspCode resp) ++ " " ++ rspReason resp
+    return resp
 
 request :: URI -> URIAuth -> [Header] -> C.ByteString -> Request C.ByteString
 request uri auth usrHeaders content = Request{ rqURI = uri,
@@ -94,6 +150,38 @@ request uri auth usrHeaders content = Request{ rqURI = uri,
     headers = [Header HdrUserAgent userAgent,
                Header HdrContentType "text/xml",
                Header HdrContentLength (show (C.length content))
+              ] ++ maybeToList (uncurry authHdr . parseUserInfo $ auth)
+                ++ usrHeaders
+    parseUserInfo info = let (u,pw) = break (==':') $ uriUserInfo info
+                         in ( if null u then Nothing else Just u
+                            , if null pw then Nothing else Just (tail pw))
+
+get :: String -> [Header] -> [Cookie] -> IO (Response U.ByteString)
+get url headers cookies = do
+    uri <- maybeFail ("Bad URI: '" ++ url ++ "'") (parseURI url)
+    let a = uriAuthority uri
+    auth <- maybeFail ("Bad URI authority: '" ++ show (fmap showAuth a) ++ "'") a
+    get_ uri auth headers'
+  where showAuth (URIAuth u r p) = "URIAuth "++u++" "++r++" "++p
+        headers' = Header HdrCookie (concat $ intersperse ";" $ map showCookie cookies) : headers
+
+get_ :: URI -> URIAuth -> [Header] -> IO (Response U.ByteString)
+get_ uri auth headers = do
+    eresp <- simpleHTTP (request' uri auth headers)
+    resp <- handleE (fail . show) eresp
+    return resp
+
+request' :: URI -> URIAuth -> [Header] -> Request C.ByteString
+request' uri auth usrHeaders = Request
+  { rqURI = uri
+  , rqMethod = GET
+  , rqHeaders = headers
+  , rqBody = ""
+  }
+    where
+    -- the HTTP module adds a Host header based on the URI
+    headers = [Header HdrUserAgent userAgent,
+               Header HdrContentType "text/xml"
               ] ++ maybeToList (uncurry authHdr . parseUserInfo $ auth)
                 ++ usrHeaders
     parseUserInfo info = let (u,pw) = break (==':') $ uriUserInfo info
